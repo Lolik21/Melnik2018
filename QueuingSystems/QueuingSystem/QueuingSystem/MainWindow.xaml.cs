b92240a9caeb26990957b3f9d57eb85cde6d79e4
Ilya Kremniou <Ilya_Kremniou@epam.com>
2018-10-01 07:45:02 +0300

    Lab 4 done

--- QueuingSystems/QueuingSystem/QueuingSystem/MainWindow.xaml.cs
+++ QueuingSystems/QueuingSystem/QueuingSystem/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace QueuingSystem
 {
@@ -21,24 +24,78 @@ namespace QueuingSystem
     public partial class MainWindow : Window
     {
         private const int BambardCount = 100000;
+        private string Result = "";
+        private int I = 0;
+        private Timer timer = null; 
         public MainWindow()
         {
             InitializeComponent();
             OperationProgressBar.Maximum = BambardCount;
+            timer = new Timer(50);
+            timer.Elapsed += TimerOnElapsed;
         }
 
-        private async void ButtonCalculate_Click(object sender, RoutedEventArgs e)
-        {
-            var system = new QueuingSystemClass();
-            await Task.Run(() =>
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {           
+            var rez = "";
+            var i = 0;
+            lock (this)
             {
-                for (var i = 0; i < BambardCount; i++)
+                if (Result == "") return;
+                rez = Result + " ";
+                i = I;
+                Result = "";
+            }
+
+            Dispatcher.Invoke(() => UpdateTextBox(i, rez));
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TextBoxOutPut.Clear();
+                Result = "";
+                I = 0;
+                var pi1 = double.Parse(Pi1TextBox.Text, CultureInfo.InvariantCulture);
+                var pi2 = double.Parse(Pi2TextBox.Text, CultureInfo.InvariantCulture);
+                var system = new QueuingSystemClass
                 {
-                    system.ProcessRequest();
-                    Dispatcher.Invoke(() => OperationProgressBar.Value = i);
-                }
-            });
+                    Pi1 = pi1,
+                    Pi2 = pi2
+                };
+                Task.Run(() =>
+                {
+                    for (var i = 0; i < BambardCount; i++)
+                    {
+                        lock (this)
+                        {
+                            Result = Result + system.ProcessTick();
+                            I = i;                           
+                        }
+                        timer.Start();
+                    }
+                    system.CalcStats(out var A, out var P,out var L, BambardCount);
+                    Dispatcher.Invoke(() =>
+                    {
+                        LabelAbsoluteProbability.Content = A.ToString(CultureInfo.InvariantCulture);
+                        LabelProbabilityOfRegectiong.Content = P.ToString(CultureInfo.InvariantCulture);
+                        LabelQueryLength.Content = L.ToString(CultureInfo.InvariantCulture);
+                    });
+                });
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
             
+            
+        }
+
+        private void UpdateTextBox(int i, string rez)
+        {
+            TextBoxOutPut.AppendText(rez);
+            OperationProgressBar.Value = i;
         }
     }
 }
--- QueuingSystems/QueuingSystem/QueuingSystem/QueuingSystem.cs
+++ QueuingSystems/QueuingSystem/QueuingSystem/QueuingSystem.cs
@@ -8,10 +8,15 @@ namespace QueuingSystem
 {
     public class QueuingSystemClass
     {
-        private const int BufferSize = 2;
-        private const int TiksToNewRequest = 2;
+        private static Random Random = new Random(DateTime.Now.Millisecond);
 
-        private int _regectedRequestsCount = 0;
+        private const int BufferSize = 2;
+        private const int TickToNewRequest = 2;
+
+        public double Pi1 { get; set; } = 0.5;
+        public double Pi2 { get; set; } = 0.5;
+
+        private int _rejectedRequestsCount = 0;
         private int _processedRequestsCount = 0;
 
         private int _currentTicksState = 2;
@@ -19,9 +24,154 @@ namespace QueuingSystem
         private int _currentPi1State = 0;
         private int _currentPi2State = 0;
         
-        public void ProcessRequest()
-        {
+        private List<int> bufferLength = new List<int>();
+        private List<string> buf = new List<string>();
 
+        public string ProcessTick()
+        {
+            _currentTicksState--;
+            AddTick();
+            if (_currentTicksState == 0)
+            {
+                _currentTicksState = 2;
+                AddRequest();
+            }
+
+            bufferLength.Add(_currentBufferState);
+            var str = $"P{_currentTicksState}{_currentBufferState}{_currentPi1State}{_currentPi2State}";
+            buf.Add(str);
+            return str + " --> ";
+        }
+
+        public void CalcStats(out double A, out double P, out double L, int bambard)
+        {
+            //var rez = buf.OrderBy(s => s).GroupBy(s => s).Select(grouping =>
+            //{
+            //    var r = ((double)grouping.Count() / bambard);
+            //    return new {grouping.Key, r};
+            //}).ToList();
+            A = (double)_processedRequestsCount / bambard;
+            P = (double)_rejectedRequestsCount / bambard * 2;
+            L = (double)bufferLength.Sum() / bambard;
+        }
+
+        private void AddTick()
+        {
+            if (_currentPi1State == 0 && _currentPi2State == 0)
+            {
+                //TODO:dosmth
+                return;
+            }
+
+            if (_currentPi1State == 1 && _currentPi2State == 0)
+            {
+                switch (Process2Probability(Pi1, 1 - Pi1))
+                {
+                    case 1:
+                        return;
+                    case 2:
+                        _currentPi1State--;
+                        _currentPi2State++;
+                        if (_currentBufferState > 0)
+                        {
+                            _currentBufferState--;
+                            _currentPi1State++;
+                        }
+                        break;
+                }
+                return;
+            }
+
+            if (_currentPi1State == 0 && _currentPi2State == 1)
+            {
+                switch (Process2Probability(Pi2, 1 - Pi2))
+                {
+                    case 1:
+                        return;
+                    case 2:
+                        _currentPi2State--;
+                        _processedRequestsCount++;
+                        break;
+                }
+                return;
+            }
+
+            if (_currentPi1State == 1 && _currentPi2State == 1)
+            {
+                var state1 = Process2Probability(Pi1, 1 - Pi1);
+                var state2 = Process2Probability(Pi2, 1 - Pi2);
+
+                if (state1 == 1 && state2 == 1)
+                {
+                    return;
+                }
+
+                if (state1 == 1 && state2 == 2)
+                {
+                    _currentPi2State--;
+                    _processedRequestsCount++;
+                    return;
+                }
+
+                if (state1 == 2 && state2 == 1)
+                {
+                    _currentPi1State--;
+                    _rejectedRequestsCount++;
+                    if (_currentBufferState > 0)
+                    {
+                        _currentBufferState--;
+                        _currentPi1State++;
+                    }
+                    return;
+                }
+
+                if (state1 == 2 && state2 == 2)
+                {
+                    _currentPi1State--;
+                    _processedRequestsCount++;
+                    if (_currentBufferState > 0)
+                    {
+                        _currentBufferState--;
+                        _currentPi1State++;
+                    }
+                    return;
+                }
+            }
+        }
+
+        private void AddRequest()
+        {
+            if (_currentPi1State == 0)
+            {
+                _currentPi1State++;
+                return;
+            }
+
+            if (_currentPi1State == 1 && _currentBufferState == 0)
+            {
+                _currentBufferState++;
+                return;
+            }
+
+            if (_currentPi1State == 1 && _currentBufferState == 1)
+            {
+                _currentBufferState++;
+                return;
+            }
+
+            _rejectedRequestsCount++;
+        }
+
+
+        private int Process2Probability(double p1, double p2)
+        {
+            var randomValue = Random.Next(101);
+            if (randomValue <= p1 * 100)
+            {
+                return 1;
+            }
+
+            return 2;
         }
     }
 }
