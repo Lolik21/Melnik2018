e951c3c4cd2fb0d7183ed293c06f67715376199c
Ilya Kremniou <Ilya_Kremniou@epam.com>
2018-10-28 22:34:55 +0300

    Lab 5-6 done

---
+++ SMO/QueuingSystem/QueuingSystem/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Timer = System.Timers.Timer;
+
+namespace QueuingSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int BombardTime = 10000000;
+        private string Result = "";
+        private int I = 0;
+        private Timer timer = null; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                
+                var peopleIntensity = double.Parse(Pi1TextBox.Text, CultureInfo.InvariantCulture);
+                var carIntensity = double.Parse(Pi2TextBox.Text, CultureInfo.InvariantCulture);
+                var system = new QueuingSystemClass
+                {
+                    PeopleIntensity = peopleIntensity,
+                    CarIntensity = carIntensity
+                };
+                Task.Run(() =>
+                {
+                    Stopwatch SW = new Stopwatch();
+                    SW.Start();
+                    for (var i = 0; i < BombardTime; i++)
+                    {
+                        system.ProcessTick();
+                    }
+                    system.CalcStats(out var peopleQueryLength, out var carQueryLength,out var peopleWaitTime,
+                        out var  carWaitTime,out var chanceToLeave, BombardTime);
+                    SW.Stop();
+                    Dispatcher.Invoke(() =>
+                    {
+                        LabelPeopleQueryLength.Content = peopleQueryLength.ToString(CultureInfo.InvariantCulture);
+                        LabelVehicleQueryLength.Content = carQueryLength.ToString(CultureInfo.InvariantCulture);
+                        LabelPeopleWait.Content = peopleWaitTime.ToString(CultureInfo.InvariantCulture);
+                        LabelVehicleWait.Content = carWaitTime.ToString(CultureInfo.InvariantCulture);
+                        ProbabilityToLeave.Content = chanceToLeave.ToString(CultureInfo.InvariantCulture);
+                        TextBlock.Text = SW.Elapsed.ToString();
+                    });
+                });
+                
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }                      
+        }
+    }
+}
---
+++ SMO/QueuingSystem/QueuingSystem/QueuingSystem.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace QueuingSystem
+{
+    public class QueuingSystemClass
+    {
+        private static Random Random = new Random(DateTime.Now.Millisecond);
+
+        private const int CarBufferSize = 11;
+        private const int PeopleBufferSize = 21;
+         
+        public double PeopleIntensity { get; set; } = 12;
+        public double CarIntensity { get; set; } = 12;
+
+
+        private int _currentCarBufferSize = 0;
+        private int _currentPeopleBufferSize = 0;
+
+        private int _leavedPeople = 0;
+        private int _arrivedPeople = 0;
+        
+        private List<int> _carQueryLength = new List<int>();
+        private List<int> _peopleQueryLength = new List<int>();
+
+        public void ProcessTick()
+        {
+            AddTick();
+            _carQueryLength.Add(_currentCarBufferSize);
+            _peopleQueryLength.Add(_currentPeopleBufferSize);
+        }
+
+        public void CalcStats(out double peopleQueryLength, out double carQueryLength, out double peopleWaitTime,
+            out double varWaitTime, out double chanceToLeave,int BombardTime)
+        {
+            peopleQueryLength = _peopleQueryLength.Sum() / (double)BombardTime;
+            carQueryLength = _carQueryLength.Sum() / (double)BombardTime;
+            peopleWaitTime = peopleQueryLength / PeopleIntensity + 1/ PeopleIntensity;
+            varWaitTime = carQueryLength / CarIntensity + 1 / CarIntensity;
+
+            chanceToLeave = _leavedPeople / (double)_arrivedPeople;
+        }
+
+        private void AddTick()
+        {
+            var isCarArrived = IsEventHappened(CarIntensity);
+            var isPeopleArrived = IsEventHappened(PeopleIntensity);
+
+            if (isCarArrived && isPeopleArrived)
+            {
+                _arrivedPeople++;
+                _leavedPeople++;
+                return;
+            }
+
+            if (isPeopleArrived)
+            {
+                if (_currentCarBufferSize > 0)
+                {
+                    _currentCarBufferSize--;                    
+                    _leavedPeople++;
+                }
+                else
+                {
+                    if (PeopleBufferSize != _currentPeopleBufferSize)
+                    {
+                        _currentPeopleBufferSize++;                        
+                    }
+                }
+                _arrivedPeople++;
+                return;
+            }
+
+            if (isCarArrived)
+            {
+                if(_currentPeopleBufferSize > 0)
+                {
+                    _currentPeopleBufferSize--;
+                    _leavedPeople++;
+                }
+                else
+                {
+                    if (CarBufferSize != _currentCarBufferSize)
+                    {
+                        _currentCarBufferSize++;
+                    }
+                }               
+            }
+
+        }
+
+        public bool IsEventHappened(double intensity)
+        {
+            var randomValue = Random.Next(60);
+            if (randomValue < intensity)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
